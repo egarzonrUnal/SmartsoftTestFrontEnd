dd5f320e06f11882b0c76d22e0f4b85dc1ab982c
Ernesto <danteunal@gmail.com>
2020-08-08 16:29:06 -0500

    kendo dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { DataComponent } from './components/data/data.component';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurationComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
     ButtonsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DropDownsModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
